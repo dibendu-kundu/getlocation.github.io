98ff362772117ee0149f38a072dc7ce5fe2c7a95
Dibendu Kundu <72265630+dibendu-kundu@users.noreply.github.com>
2022-09-30 12:55:13 +0530

    Delete p21.js

--- p21.js
+++
@@ -1,4 +0,0 @@
-navigator.geolocation.getCurrentPosition(function(position){
-    document.write("Latitude= "+position.coords.latitude);
-    document.write(" Longitude= "+position.coords.longitude);
-})
